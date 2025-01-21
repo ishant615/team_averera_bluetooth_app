416b7e12db190de2f16700f4bd7bcf4a3cb350f5
ishant615 <158510078+ishant615@users.noreply.github.com>
2025-01-22 03:41:02 +0530

    Add files via upload

---
+++ src/appinventor/ai_ishant_arora_ece23/bluetooth_receiver/Screen2.scm
@@ -0,0 +1,4 @@
+#|
+$JSON
+{"authURL":["ai2.appinventor.mit.edu"],"YaVersion":"231","Source":"Form","Properties":{"$Name":"Screen2","$Type":"Form","$Version":"31","ActionBar":"True","AppName":"bluetooth_receiver","Theme":"AppTheme","Title":"Screen2","Uuid":"0","$Components":[{"$Name":"Label1","$Type":"Label","$Version":"5","FontBold":"True","Width":"-2","Text":"LS STATUS","TextAlignment":"1","Uuid":"-84947879"},{"$Name":"Map1","$Type":"Map","$Version":"6","Height":"-2","Width":"-2","ShowUser":"True","Uuid":"1147838236","$Components":[{"$Name":"Marker1","$Type":"Marker","$Version":"4","AnchorVertical":"2","Latitude":"42.37414428190191","Longitude":"-71.1193084716797","Uuid":"-523749692"}]},{"$Name":"LocationSensor1","$Type":"LocationSensor","$Version":"3","Uuid":"-1880218155"},{"$Name":"Clock1","$Type":"Clock","$Version":"4","TimerInterval":"500","Uuid":"1904015301"}]}}
+|#
